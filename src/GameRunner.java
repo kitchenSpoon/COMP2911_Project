395f9a3f239f79d7cf7c1d3c16100311f7b2afe3
jhdb <jenina.h.balaton@gmail.com>
2014-05-30 21:34:25 +1000

    Added comments

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,6 +1,12 @@
-
+/**
+ * A class containing the main method and runs the GameManager to start the Maze game
+ */
 public class GameRunner {
 
+	/**
+	 * Run the GameManager to start the maze game
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		GameManager gm = new GameManager();
 	}
